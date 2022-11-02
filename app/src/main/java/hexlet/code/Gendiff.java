9c409e247fbe55d95a629e4f63b37a65fefe213a
ags <a.g.samkin@gmail.com>
2022-11-02 21:15:35 +0300

    Add option and parameters to console app

--- app/src/main/java/hexlet/code/Gendiff.java
+++ app/src/main/java/hexlet/code/Gendiff.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -11,11 +12,20 @@ import java.util.concurrent.Callable;
 )
 public class Gendiff implements Callable<Integer> {
 
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-    boolean usageHelpRequested;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
+    private String format;
 
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-    boolean versionInfoRequested;
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.", usageHelp = true)
+    private boolean usageHelpRequested;
+
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.", versionHelp = true)
+    private boolean versionInfoRequested;
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private String filepath2;
 
     @Override
     public Integer call() throws Exception {
