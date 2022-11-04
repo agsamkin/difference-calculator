57774a9de3b8967235eface3ad27abce6456abe5
ags <a.g.samkin@gmail.com>
2022-11-04 16:16:09 +0300

    Add differ

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,44 @@
 package hexlet.code;
 
+import java.util.concurrent.Callable;
+
 import picocli.CommandLine;
 
-public class App {
+import static picocli.CommandLine.Command;
+import static picocli.CommandLine.Option;
+import static picocli.CommandLine.Parameters;
+
+@Command(
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference."
+)
+public class App implements Callable<Integer> {
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
+    private String format;
+
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.", usageHelp = true)
+    private boolean usageHelpRequested;
+
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.", versionHelp = true)
+    private boolean versionInfoRequested;
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private String filepath2;
+
+    @Override
+    public Integer call() throws Exception {
+        String diff = Differ.generate(filepath1, filepath2);
+        System.out.println(diff);
+        return 0;
+    }
+
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new Gendiff()).execute(args);
+        int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
+
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,58 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Differ {
-    public static String generate(String filePath1, String filePath2) {
-        return "";
+    public static String generate(String filePath1, String filePath2) throws Exception {
+
+        Path path1 = Paths.get(filePath1).toAbsolutePath().normalize();
+        if (!Files.exists(path1)) {
+            throw new Exception("File '" + path1 + "' does not exist");
+        }
+
+        Path path2 = Paths.get(filePath2).toAbsolutePath().normalize();
+        if (!Files.exists(path2)) {
+            throw new Exception("File '" + path2 + "' does not exist");
+        }
+
+        String content1 = Files.readString(path1);
+        String content2 = Files.readString(path2);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map1 = mapper.readValue(content1, TreeMap.class);
+        Map<String, Object> map2 = mapper.readValue(content2, TreeMap.class);
+        return getDiff(map1, map2);
+
     }
+
+    private static String getDiff(Map<String, Object> map1, Map<String, Object> map2) {
+        Set<String> keys = Stream.concat(map1.keySet().stream(), map2.keySet().stream())
+                .collect(Collectors.toCollection(TreeSet::new));
+
+        StringBuilder diff = new StringBuilder("{\n");
+        for (String key : keys) {
+            if (map1.containsKey(key) && map2.containsKey(key)) {
+                if (map1.get(key).equals(map2.get(key))) {
+                    diff.append("  ").append(key).append(": ").append(map1.get(key)).append("\n");
+                } else {
+                    diff.append("- ").append(key).append(": ").append(map1.get(key)).append("\n");
+                    diff.append("+ ").append(key).append(": ").append(map2.get(key)).append("\n");
+                }
+            } else if (map1.containsKey(key)) {
+                diff.append("- ").append(key).append(": ").append(map1.get(key)).append("\n");
+            } else {
+                diff.append("+ ").append(key).append(": ").append(map2.get(key)).append("\n");
+            }
+        }
+        diff.append("}");
+        return diff.toString();
+    }
+
 }
