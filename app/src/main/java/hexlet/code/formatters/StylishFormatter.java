34141489bb6a18b7b67058bbf70073b6fe25a530
ags <a.g.samkin@gmail.com>
2022-11-11 21:08:17 +0300

    Fix stylish formatter

--- app/src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -5,6 +5,7 @@ import hexlet.code.DiffElement;
 import java.util.List;
 
 public final class StylishFormatter implements Formatter {
+    public static final int SPACE_COUNT = 4;
     @Override
     public String format(List<DiffElement> diff) {
         StringBuilder sb = new StringBuilder("{\n");
@@ -14,7 +15,7 @@ public final class StylishFormatter implements Formatter {
             } else if (DiffElement.Type.REMOVED.equals(diffElement.diffElementType())) {
                 sb.append(" ".repeat(2)).append("- ");
             } else {
-                sb.append(" ".repeat(4));
+                sb.append(" ".repeat(SPACE_COUNT));
             }
             sb.append(diffElement.key()).append(": ").append(diffElement.value()).append("\n");
         }
