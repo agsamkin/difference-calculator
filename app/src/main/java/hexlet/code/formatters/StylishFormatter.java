34ae41bd852a28c3a905b850e456d6aff77d9541
ags <a.g.samkin@gmail.com>
2022-11-11 20:56:42 +0300

    Fix stylish formatter

--- app/src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -10,11 +10,11 @@ public final class StylishFormatter implements Formatter {
         StringBuilder sb = new StringBuilder("{\n");
         for (DiffElement diffElement : diff) {
             if (DiffElement.Type.ADDED.equals(diffElement.diffElementType())) {
-                sb.append("+ ");
+                sb.append(" + ");
             } else if (DiffElement.Type.REMOVED.equals(diffElement.diffElementType())) {
-                sb.append("- ");
+                sb.append(" - ");
             } else {
-                sb.append(" ".repeat(2));
+                sb.append(" ".repeat(3));
             }
             sb.append(diffElement.key()).append(": ").append(diffElement.value()).append("\n");
         }
