2b5226b221ff228172da744a43f53c9bd03ceacd
ags <a.g.samkin@gmail.com>
2022-11-18 20:20:08 +0300

    Refactoring

--- app/src/main/java/hexlet/code/DiffElement.java
+++ app/src/main/java/hexlet/code/DiffElement.java
@@ -1,8 +1,27 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 public record DiffElement(Type diffElementType, Object value) {
+
+    @JsonFormat(shape = JsonFormat.Shape.OBJECT)
     public enum Type {
-        ADDED, REMOVED, NOT_CHANGED
+        ADDED("added"),
+        REMOVED("removed"),
+        NOT_CHANGED("not_changed");
+
+        private String formatted;
+
+        Type(String formatted) {
+            this.formatted = formatted;
+        }
+
+        public String getFormatted() {
+            return formatted;
+        }
     }
 
     public static boolean isAdded(Type type) {
--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -1,6 +1,7 @@
 package hexlet.code.formatters;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.DiffElement;
 
@@ -11,31 +12,11 @@ import java.util.Map;
 public final class JsonFormatter implements Formatter {
     @Override
     public String format(Map<String, List<DiffElement>> diff) {
-        Map<String, Object> result = new LinkedHashMap<>();
-        for (Map.Entry<String, List<DiffElement>> element : diff.entrySet()) {
-            String key = element.getKey();
-            List<DiffElement> values = element.getValue();
-            for (DiffElement value : values) {
-                DiffElement.Type type = value.diffElementType();
-                result.put(getKey(key, type), value.value());
-            }
-        }
-
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            return objectMapper.writeValueAsString(result);
+            return objectMapper.writeValueAsString(diff);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Error format to json", e);
-        }
-    }
-
-    private String getKey(String key, DiffElement.Type type) {
-        if (DiffElement.isAdded(type)) {
-            return "+ " + key;
-        } else if (DiffElement.isRemoved(type)) {
-            return "- " + key;
-        } else {
-            return " ".repeat(2) + key;
         }
     }
 }
